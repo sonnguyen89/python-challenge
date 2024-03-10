ecae444e6b5d56a4918d4bd12899fe9342c56f05
sonnguyen89 <nam_son14@yahoo.com>
2024-03-11 02:44:01 +1100

    initialize the repository

---
+++ main.py
@@ -0,0 +1,193 @@
+import csv
+from datetime import datetime
+
+def read_csv_to_dict(file_path):
+    data_dict = {}
+    
+    with open(file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        
+        for row in csvreader:
+            # Convert "month-year" to datetime format
+            date_str = row['Date']
+            date_obj = datetime.strptime(date_str, '%b-%y')
+            
+            # Store in the dictionary
+            data_dict[date_obj] = float(row['Profit/Losses'])
+    
+    return data_dict
+
+def calculate_sum_profits_months(data_dict):
+    profits = []
+    months =[]   
+    # Sort the dictionary by date
+    sorted_data = sorted(data_dict.items(), key=lambda x: x[0])
+    for i in range(0, len(sorted_data)):
+        current_date, current_profit = sorted_data[i]
+        profits.append(current_profit)
+        months.append(current_date)
+    # Calculate the sum of profit/loss        
+    return int(len(months)),sum(profits)
+
+
+def calculate_changes_and_average(data_dict):
+    changes = []
+    
+    # Sort the dictionary by date
+    sorted_data = sorted(data_dict.items(), key=lambda x: x[0])
+
+    # Calculate changes in Profit/Losses
+    for i in range(1, len(sorted_data)):
+        current_date, current_profit = sorted_data[i]
+        prev_date, prev_profit = sorted_data[i - 1]
+        change = current_profit - prev_profit
+        changes.append(change)
+
+    # Calculate the average of changes
+    average_change = sum(changes) / len(changes)
+
+    return changes, average_change
+
+
+def calculate_greatest_increase_and_decrease(data_dict):
+    sorted_data = sorted(data_dict.items(), key=lambda x: x[0])
+
+    greatest_increase_date, greatest_increase_amount = None, float('-inf')
+    greatest_decrease_date, greatest_decrease_amount = None, float('inf')
+    change = 0
+    for i in range(1, len(sorted_data)):
+        current_date, current_profit = sorted_data[i]
+        prev_date, prev_profit = sorted_data[i - 1]
+        change = current_profit - prev_profit
+
+        if change > greatest_increase_amount:
+            greatest_increase_amount = change
+            greatest_increase_date = current_date
+
+        if change < greatest_decrease_amount:
+            greatest_decrease_amount = change
+            greatest_decrease_date = current_date
+
+    return greatest_increase_date.strftime('%b-%y'), greatest_increase_amount, greatest_decrease_date.strftime('%b-%y'), greatest_decrease_amount
+
+
+def print_bank_results_to_text_file(months_total, profits_total,average_change, greatest_increase_date, greatest_increase_amount, greatest_decrease_date, greatest_decrease_amount, output_file):
+    with open(output_file, 'w') as text_file:
+        # Write header
+        text_file.write("Financial Analysis\n")
+        text_file.write("----------------------------\n")
+
+        # Write results
+        text_file.write(f"Total Months: {months_total}\n")
+        text_file.write(f"Total: ${profits_total:.2f}\n")
+        text_file.write(f"Average Change: ${average_change:.2f}\n")
+        text_file.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount:.0f})\n")
+        text_file.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount:.0f})\n")
+
+
+def run_PyBank():
+    # run the functions to process result from the CSV file
+    csv_file_path = 'Resources/budget_data.csv'
+   
+    data_dict = read_csv_to_dict(csv_file_path)
+
+    # run Total Profits and total months function
+    months_total,profits_sum = calculate_sum_profits_months(data_dict)
+
+    # run  average change function
+    changes, average_change = calculate_changes_and_average(data_dict)
+    # run greates increase and decreasted amount function
+    greatest_increase_date, greatest_increase_amount, greatest_decrease_date, greatest_decrease_amount = calculate_greatest_increase_and_decrease(data_dict)
+
+    # Save result to the text file budget_data.txt
+    output_file_path = 'analysis/budget_data.txt'
+    print_bank_results_to_text_file(months_total, profits_sum,average_change, greatest_increase_date, greatest_increase_amount, greatest_decrease_date, greatest_decrease_amount, output_file_path)
+
+    # Print the results in terminal for testing
+    #print(f'Total Months: {months_total}')
+    #print(f'Total: ${profits_sum}')
+    #print(f'Changes in Profit/Losses over the entire period: {changes}')
+    #print(f'Average Change: ${average_change}')
+    #print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})')
+    #print(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})')
+
+####################################################################
+
+
+def analyze_poll(csv_file_path):
+    total_votes = 0
+    candidates_votes = {}
+    with open(csv_file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+
+        for row in csvreader:
+            total_votes += 1
+
+            candidate_name = row['Candidate']
+
+            # Update candidate name not in the candiates list, 
+            # then add one and set poll value = 1 for the starting one
+            if candidate_name not in candidates_votes:
+                candidates_votes[candidate_name] = 1
+            else:
+                candidates_votes[candidate_name] += 1 #add one vote when found candidate name in the list
+
+    return total_votes,candidates_votes
+
+def calculate_percentages(candidate_votes, total_votes):
+    percentages = {}
+
+    for candidate_name, votes in candidate_votes.items():
+        percentage = (votes * 100) / total_votes
+        percentages[candidate_name] = round(percentage,3)
+
+    return percentages
+
+def print_poll_results_to_text_file(total_votes,candidate_votes,percentages,output_file):
+    with open(output_file, 'w') as text_file:
+        # Write header
+        text_file.write("Election Results\n")
+        text_file.write("----------------------------\n")
+        text_file.write(f"Total Votes: {total_votes}\n")
+        text_file.write("----------------------------\n")
+        for candidate_name, votes in percentages.items():
+            text_file.write(f"{candidate_name}: {votes}% ({candidate_votes[candidate_name]})\n")
+
+        text_file.write("----------------------------\n")
+        
+        winner = max(percentages, key=percentages.get)
+        text_file.write(f"Winner: {winner}\n")
+
+        text_file.write("----------------------------\n")
+
+def run_PyPoll():    
+    csv_file_path = 'Resources/election_data.csv'
+    total_votes,candidate_votes  = analyze_poll(csv_file_path)
+    percentages = calculate_percentages(candidate_votes, total_votes)
+
+    # Save result to the text file election_data.txt
+    output_file_path = 'analysis/election_data.txt'
+    print_poll_results_to_text_file(total_votes, candidate_votes, percentages,output_file_path)
+
+    # Print the results in terminal for testing
+    # print(f"Total Votes: {total_votes}")
+    # print(f'{candidate_votes}')
+    # print(f'{percentages}')
+
+
+
+########################################################
+
+
+# Python script to analyse the financial records
+run_PyBank()
+
+# Python script to analyse the financial records
+run_PyPoll()
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,62 @@
+Python Challenge Repository
+
+This repository contains 2 Python scripts for read the csv input file, analyse the financial records and vote-counting records then export all the result to text file .
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Usage](#usage)
+- [Scripts](#scripts)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+## Overview
+Python script that analyses the finance records and calculate each of the following values:
+   1. The total number of months included in the dataset
+   2. The net total amount of "Profit/Losses" over the entire period
+   3. The changes in "Profit/Losses" over the entire period, and then the average of those changes
+   4. The greatest increase in profits (date and amount) over the entire period
+   5. The greatest decrease in profits (date and amount) over the entire period
+
+a Python script that analyzes the votes and calculates each of the following values:
+   1. The total number of votes cast
+   2. A complete list of candidates who received votes
+   3. The percentage of votes each candidate won
+   4. The total number of votes each candidate won
+   5. The winner of the election based on popular vote
+## Usage
+
+To use the scripts in your Mac or Window environment, follow these general steps:
+
+1. Open Terminal(Mac OS) or Command Promp (Windows).
+2. cd to directory "python-challenge"
+3. run command "python main.py".
+4. check the output files at 'python-challenge/analysis' you would see 2 result text files were generated
+
+
+## Scripts
+
+Python scripts available in this repository:
+
+- `main.py`: A script to read the csv files,  analyse and print the result in text file.
+
+
+Feel free to explore and modify these scripts to suit your specific needs.
+
+## Contributing
+
+Contributions to this repository are welcome! If you have any useful VBS scripts or improvements to existing ones, please feel free to submit a pull request.
+
+Before contributing, please ensure that your code adheres to the repository's coding standards and practices.
+
+## License
+
+This repository is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+If you have any questions, suggestions, or concerns regarding this repository, please don't hesitate to contact the repository owner:
+
+- Email: [nam_son14@yahoo.com](mailto:nam_son14@yahoo.com
+- GitHub: [songuyen89](https://github.com/sonnguyen89)
